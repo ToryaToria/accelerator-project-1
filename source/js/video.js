e3ca36c13ecc650090aebecdce90dec07df13c9a
ToryaToria <toria.torya@yandex.ru>
2026-02-24 13:29:46 +0300

    Updates

--- source/js/video.js
+++
@@ -1,9 +0,0 @@
-const video = document.getElementById('my-video');
-const play = document.querySelector('.video__play');
-
-const playStop = () => {
-  video.src = video.dataset.src;
-  play.style.display = 'none';
-};
-
-play.addEventListener('click', playStop);
